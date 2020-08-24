727cef7e776b548c382aa77925023de8ae27e566
RossGosky <goskyrm@appstate.edu>
2020-08-24 08:49:32 -0400

    new rscript2 and change to rscript

--- rscript.R
+++ rscript.R
@@ -1,3 +1,12 @@
 x <- c(1,2,3)
 y <- c(4,5,6)
 hist(x)
+
+
+library(tidyverse)
+
+ggplot(dota = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy))
+
+fliter(mpg, cyl = 8)
+filter(diamond, carat > 3)
